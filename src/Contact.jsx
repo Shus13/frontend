af9ed10c4be61ae620bf276e354e8771be975f6a
Shus13 <sushitkarki234@gmail.com>
2025-05-10 22:56:28 +0545

    Adding Contact page

---
+++ src/Contact.jsx
@@ -0,0 +1,14 @@
+
+function Contact() {
+    return (
+        <div>
+            <h1>This is Contact Page</h1>
+            <h2>This is contact page</h2>
+            <p>Here is contact Information</p>
+            <p>Contact No : 9816543474</p>
+            <a href="/about">Go to about page</a>
+        </div>
+    )
+}
+
+export default Contact
