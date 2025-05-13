6f787d9e2274b36e10f65a4d03e2f8a8f1b60640
Shus13 <sushitkarki234@gmail.com>
2025-05-13 18:47:41 +0545

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Home"
 import About from "./About"
 import Contact from "./Contact"
+import LearningUseEffect from "./LearningUseEffect"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/useeffect" element={<LearningUseEffect/>}/>
+
       </Routes>
     </BrowserRouter>
   )
